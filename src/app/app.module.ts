c2297c37f27ba3f458375c67679e0a152f4e5c27
damirpie <damirdamir692@gmail.com>
2023-09-21 12:41:09 +0600

    added new post2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {PostComponent} from "../post/post.component";
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule
