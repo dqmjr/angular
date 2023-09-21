84a42338b92fe7b5f86bf49525c06e04ab6e4a5c
damirpie <damirdamir692@gmail.com>
2023-09-21 14:56:10 +0600

    Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {timeout} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ export class AppComponent {
   number = 223
   arr = [21, 32 ,33, 44]
   obj = { a: 14, b: { c:23 } }
+
+  img = 'https://stat.gov.kz/local/templates/statgov_main/img/bns_logo.svg'
+
+  constructor() {
+    setTimeout(() => {
+    console.log('Time is over')
+    this.img = 'https://static.tildacdn.com/tild3864-3939-4230-b465-323933643939/DataQazStat_21.png'
+    }, 5000)
+  }
 }
